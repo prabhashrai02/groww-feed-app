17a88eeea3ba4a8c40941762df4c0a1fc6254b7d
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 00:47:06 +0530

    created card for image data

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,8 +3,8 @@ function Card(props) {
     const data = props.data;
     return (
         <div key={data.id} className="card flex flex_direction_column">
-            <img src={data.urls.thumb} alt={data.alt_description}/>
-            <div className="flex card_description justify_content_space_between">
+            <img src={data.urls.small} alt={data.alt_description} className="data_img"/>
+            <div className="flex card_description justify_content_space_around">
                 <p><a>{data.user.instagram_username}</a></p>
                 <p>{data.likes}</p>
             </div>
