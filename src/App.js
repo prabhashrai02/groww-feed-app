3c034edeffabbb76bf2c9ce7faca929f2f2fe02c
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-01 14:14:56 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Feed from './Components/Feed/Feed';
 import Profile from './Components/Profile/Profile';
+import { Routes, Route, Navigate, HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Feed />
-      <Profile />
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>        
+          <Route exact path='/feed' element={<Feed />} />
+          <Route exact path='/profile' element={<Profile />} />
+          <Route path="*" element={<Navigate replace to="/Feed" />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
