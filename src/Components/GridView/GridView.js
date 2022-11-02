84424bbb178ea41d1c6a42f3166aa31e33d15344
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 12:15:44 +0530

    created grid view

---
+++ src/Components/GridView/GridView.js
@@ -0,0 +1,26 @@
+import Card from "../Card/Card";
+
+function GridView(props) {
+  let state = [];
+  if (props.data) state = props.data;
+  
+    const imageList = state.map((data) => <Card data={data} key={data.id}/>);
+
+    return (
+        <div className="gridView flex flex_wrap_wrap align_items_center">
+        {
+          imageList.map(
+            (project, index) => {
+              return (
+                <div key={index}>
+                  {project}
+                </div>
+              )
+            }
+          )
+        }
+        </div>
+    );
+}
+
+export default GridView;
