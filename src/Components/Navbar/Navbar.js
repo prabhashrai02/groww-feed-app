9bdf2ee855ce58f11dda413ed75e3d3e481662fa
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 16:33:30 +0530

    fixed navbar icon button

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 function Navbar() {
     const [darkTheme, setdarkTheme] = useState(false);
@@ -16,13 +17,21 @@ function Navbar() {
         }
     }
 
+    function scrollToTop() {
+        window.scrollTo(0, 0);
+    }
+
     return (
-        <div className="navbar flex justify_content_space_around">
-            <div className="flex">
-                <img className="navbar_logo" src="https://play-lh.googleusercontent.com/7q2dwnqAFr91NBSBRGcE1tZQCJL-FYbUKEuy103bTmQowLl3yNY73ozy5cf1mso4pCS4=w600-h300-pc0xffffff-pd"/>
-                Groww Feed App
-            </div>
+        <div className="navbar flex justify_content_center">
+            <div className="navbar_container flex justify_content_space_between">
+            <Link to="/feed" onClick={scrollToTop}>
+                <div className="flex cursor">
+                    <img className="navbar_logo" src="https://play-lh.googleusercontent.com/7q2dwnqAFr91NBSBRGcE1tZQCJL-FYbUKEuy103bTmQowLl3yNY73ozy5cf1mso4pCS4=w600-h300-pc0xffffff-pd"/>
+                    Groww Feed App
+                </div>
+            </Link>
             <button className="theme_button" onClick={changeTheme}>{theme}</button>
+            </div>
         </div>
     );
 }
