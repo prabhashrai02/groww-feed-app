81473154e1b6a3786e5f8a9b9b1244349c95ff46
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 17:16:43 +0530

    fixed navbar alignment

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -26,13 +26,13 @@ function Navbar() {
     return (
         <div className="navbar flex justify_content_center">
             <div className="navbar_container flex justify_content_space_between">
-            <Link to="/feed" onClick={scrollToTop}>
-                <div className="flex cursor">
-                    <img className="navbar_logo" src="https://play-lh.googleusercontent.com/7q2dwnqAFr91NBSBRGcE1tZQCJL-FYbUKEuy103bTmQowLl3yNY73ozy5cf1mso4pCS4=w600-h300-pc0xffffff-pd"/>
-                    Groww Feed App
-                </div>
-            </Link>
-            <button className="theme_button" onClick={changeTheme}>{theme}</button>
+                <Link to="/feed" onClick={scrollToTop} >
+                    <div className="flex cursor brand_logo align_items_center">
+                        <img className="navbar_logo" src="https://play-lh.googleusercontent.com/7q2dwnqAFr91NBSBRGcE1tZQCJL-FYbUKEuy103bTmQowLl3yNY73ozy5cf1mso4pCS4=w600-h300-pc0xffffff-pd"/>
+                        Groww Feed App
+                    </div>
+                </Link>
+                <button className="theme_button" onClick={changeTheme}>{theme}</button>
             </div>
         </div>
     );
