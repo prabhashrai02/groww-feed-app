453d386101c79907c46448833f2f9aee6470e218
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 17:33:55 +0530

    fixed brand logo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -28,8 +28,7 @@ function Navbar() {
             <div className="navbar_container flex justify_content_space_between">
                 <Link to="/feed" onClick={scrollToTop} >
                     <div className="flex cursor brand_logo align_items_center">
-                        <img className="navbar_logo" src="https://play-lh.googleusercontent.com/7q2dwnqAFr91NBSBRGcE1tZQCJL-FYbUKEuy103bTmQowLl3yNY73ozy5cf1mso4pCS4=w600-h300-pc0xffffff-pd"/>
-                        Groww Feed App
+                        <img className="navbar_logo" src="https://storage.googleapis.com/groww-assets/web-assets/img/website-logo/logo-dark-groww.svg"/>
                     </div>
                 </Link>
                 <button className="theme_button" onClick={changeTheme}>{theme}</button>
