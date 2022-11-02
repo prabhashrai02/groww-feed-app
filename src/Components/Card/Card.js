3995d3773361c52549c04d89b376d8f49257c03a
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 13:14:58 +0530

    added dynamic routing

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,23 +5,27 @@ import { userNameChanged, fetchUser } from "../../Redux/Slice";
 
 function Card(props) {
     let data = [];
-    if (props.data) data = props.data;
-
+    let url = '';
+    if (props.data) {
+        data = props.data;
+        url = `/profile/${data?.user?.username}`;
+    }
     const dispatch = useDispatch();
 
     function changeUser() {
         dispatch(userNameChanged(data.user.username));
+        console.log(url)
         dispatch(fetchUser());
     }
-    
+
     return (
         <div key={data?.id} className="card flex flex_direction_column">
             <img src={data?.urls?.small} alt={data?.alt_description} className="card_image"/>
                 {
-                    data.user && data.likes && (
+                    data && data.user && (
                     <div className="flex card_description justify_content_space_around">
                         <p>
-                            <Link to="/profile">
+                            <Link to={url}>
                             <span onClick={changeUser}>{data?.user?.username}</span>
                             </Link>
                             </p>
