6dfa0b741d3cbb60b79160e0829c754f10894902
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 17:39:59 +0530

    fixed navbar alignment

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -28,7 +28,8 @@ function Navbar() {
             <div className="navbar_container flex justify_content_space_between">
                 <Link to="/feed" onClick={scrollToTop} >
                     <div className="flex cursor brand_logo align_items_center">
-                        <img className="navbar_logo" src="https://storage.googleapis.com/groww-assets/web-assets/img/website-logo/logo-dark-groww.svg"/>
+                        <img className="navbar_logo" src="https://groww.in/logo-groww270.png"/>
+                        Groww Feed App
                     </div>
                 </Link>
                 <button className="theme_button" onClick={changeTheme}>{theme}</button>
