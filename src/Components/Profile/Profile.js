7f44e41c5f5a6aed033a9946620cef57f8af77d5
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-02 09:32:49 +0530

    fetched userdata

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,16 @@
 import "./Profile.css";
+import { useSelector } from "react-redux";
 
 function Profile() {
+    const state = useSelector((state) => state.feedData)
+
     return (
         <div className="profile">
-
+            <button onClick={print}>load karo</button>
+            <div className="user_detail">
+            </div>
+            <div className="user_photos">
+            </div>
         </div>
     );
 }
--- src/Redux/Slice.js
+++ src/Redux/Slice.js
@@ -3,6 +3,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 const initialState = {
     pageNumber: 0,
     imageList: [],
+    userName: 'me',
+    userDetail: {},
 };
 
 export const fetchImages = createAsyncThunk(
@@ -18,20 +20,40 @@ export const fetchImages = createAsyncThunk(
         return result;
 })
 
+export const fetchUser = createAsyncThunk(
+    `fetchUser`, 
+    async (_ , thunkAPI) => {
+        const state = thunkAPI.getState();
+        const startURL = `https://api.unsplash.com/users/`;
+        const userName = String(state.feedData.userName);
+        const endURL = `?client_id=V4w_mAMz--_6DNlrFggMb4pq715Si8LRyjqBSImmQoM`;
+        
+        const response = await fetch(startURL + userName + endURL)
+        const result = await response.json()
+        return result;
+})
+
 export const Slice = createSlice({
     name: "feedData",
     initialState,
     reducers: {
         nextPage: (state) => {
             state.pageNumber += 1;
+        },
+        userNameChanged: (state, arg) => {
+            state.userName = arg.payload;
         }
     },
     extraReducers: {
         [fetchImages.fulfilled]: (state, action) => {
-            state.imageList = [...state.imageList, ...action.payload]
-        }
+            state.imageList = [...state.imageList, ...action.payload];
+        },
+
+        [fetchUser.fulfilled]: (state, action) => {
+            state.userDetail = JSON.parse(JSON.stringify(action.payload));
+        },
     }
 });
 
-export const { nextPage } = Slice.actions;
+export const { nextPage, userNameChanged } = Slice.actions;
 export default Slice.reducer;
