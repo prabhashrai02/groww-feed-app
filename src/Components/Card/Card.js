263c60e03f01be9b48474895b322879680abc42b
Prabhash Rai <prabhashsahaj@gmail.com>
2022-11-03 09:16:47 +0530

    implemented lazyloading with blurhash

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,14 +3,26 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { CgProfile } from "react-icons/cg";
 import { AiOutlineLike } from "react-icons/ai";
-import { userNameChanged, fetchUser } from "../../Redux/Slice";
+import { userNameChanged } from "../../Redux/Slice";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { decode } from "blurhash";
 
 function Card(props) {
     let data = [];
     let url = '';
+    
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.createImageData(240, 288);
+    let pixels = '';
+    
     if (props.data) {
         data = props.data;
         url = `/profile/${data?.user?.username}`;
+        
+        pixels = decode(data?.blur_hash, 240, 288);
+        imageData.data.set(pixels);
+        ctx.putImageData(imageData, 0, 0);
     }
     const dispatch = useDispatch();
 
@@ -20,25 +32,30 @@ function Card(props) {
 
     return (
         <div key={data?.id} className="card flex flex_direction_column">
-            <img src={data?.urls?.small} alt={data?.alt_description} className="card_image"/>
-                {
-                    data && data.user && (
-                    <div className="flex card_description align_items_center justify_content_space_around">
-                        <div>
-                            <Link to={url}>
-                                <div className='flex align_items_center'>
-                                    <CgProfile className='user_icons' />
-                                    <span onClick={changeUser}>{data?.user?.username}</span>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className='flex align_items_center'>
-                            <AiOutlineLike className='user_icons'/>
-                            <p>{data.likes}</p>
-                        </div>
+            <LazyLoadImage 
+                className="card_image"
+                placeholderSrc={canvas.toDataURL()}
+                src={data?.urls?.regular}
+                alt={data?.alt_description}
+            />
+            {
+                data && data.user && (
+                <div className="flex card_description align_items_center justify_content_space_around">
+                    <div>
+                        <Link to={url}>
+                            <div className='flex align_items_center'>
+                                <CgProfile className='user_icons' />
+                                <span onClick={changeUser}>{data?.user?.username}</span>
+                            </div>
+                        </Link>
                     </div>
-                    )
-                }
+                    <div className='flex align_items_center'>
+                        <AiOutlineLike className='user_icons'/>
+                        <p>{data.likes}</p>
+                    </div>
+                </div>
+                )
+            }
         </div>
     );
 }
